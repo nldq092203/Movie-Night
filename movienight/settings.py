252287d2e93ec11d9b941b5c32b9b4125f17549d
nldq092203 <lnguye220903@gmail.com>
2024-12-27 11:09:40 +0100

    Config redis server for cache

--- movienight/settings.py
+++ movienight/settings.py
@@ -330,7 +330,7 @@ class Dev(Configuration):
     CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": os.getenv('REDIS_URL', 'redis://redis:6379/0'),
+            "LOCATION": os.getenv('REDIS_URL', 'redis://redis:6379/1'),
             "OPTIONS": {
                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
             },
